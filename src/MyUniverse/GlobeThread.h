87877b09d3cce8b1cbc65b680538bb9073f30e24
Administrator <Administrator@4c95d191-cd10-4865-8b81-0088ca643448>
2013-06-10 07:00:22 +0000

    在界面上增加适当按钮
    git-svn-id: https://t.svnchina.com/svn/myuniverse/trunk@61 4c95d191-cd10-4865-8b81-0088ca643448

--- src/MyUniverse/GlobeThread.h
+++ src/MyUniverse/GlobeThread.h
@@ -18,13 +18,24 @@ enum STORY_TYPE
 class CStoryPage
 {
 public:
-    bool bEmpty;
-    bool bMovie;
-    CString pagePath;
-    STORY_TYPE storyType;
-    vector<CString> FrameNames;
-    int nFrames;
-    int nCurFrame;
+    //页面文件结构
+    bool bEmpty;//页面文件为空
+    bool bMovie;//页面是一个动画
+    CString pagePath;//页面文件的完整路径（目录+文件名）
+    STORY_TYPE storyType;//DDS,JPG,PNG,AVI,WMA,FOLDER
+
+    //序列帧专用
+    vector<CString> FrameNames;//序列帧（FOLDER）格式时，用来存放每一帧的文件名
+    int nFrames;//序列帧的总帧数
+    int nCurFrame;//当前的帧号
+
+    //动画控制
+    bool bPlaying;//播放动画
+    int frameRate;//动画播放速度
+
+    //旋转控制
+    bool bRotating;//自转
+    int rotationRate;//自转速度
 };
 
 DWORD WINAPI GlobeThread(LPVOID lpParam);
