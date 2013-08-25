0731f0d05539b00db9d6357cea6459b88781c726
kennyzx <kennyzx@4c95d191-cd10-4865-8b81-0088ca643448>
2013-08-25 14:23:13 +0000

    注册MyUniverseControl
    git-svn-id: https://t.svnchina.com/svn/myuniverse/trunk@143 4c95d191-cd10-4865-8b81-0088ca643448

---
+++ InnoSetup/regasm/regMyUniverseControl/regMyUniverseControl/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace regMyUniverseControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string curDir = Path.GetDirectoryName(Application.ExecutablePath);
+            string dllToRegister = Path.Combine(curDir, "MyUniverseControl.dll");
+            if (!File.Exists(dllToRegister))
+            {
+                MessageBox.Show("Can't find " + dllToRegister);
+                return;
+            }
+
+            string dirNETv2 = Path.Combine(Environment.GetEnvironmentVariable("windir"), @"Microsoft.NET\Framework\v2.0.50727");
+            if (!Directory.Exists(dirNETv2))
+            {
+                MessageBox.Show("Can't find .NET Framework 2.0"); //should not happen.
+                return;
+            }
+
+            try
+            {
+                //register dll
+                string command = Path.Combine(dirNETv2, "regasm.exe");
+                using (Process p = Process.Start(command, "\"" + dllToRegister + "\""))
+                {
+                    p.WaitForExit();
+                }
+
+                //install it to GAC
+                //command = Path.Combine(dirNETv2, "installutil.exe");
+                //using (Process p = Process.Start(command, "\"" + dllToRegister + "\""))
+                //{
+                //    p.WaitForExit();
+                //}
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error when registering MyUniverseControl.dll: " + e.Message);
+            }
+        }
+    }
+}
