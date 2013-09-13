4a40b265f650d0934355f4075404695c34d61150
kennyzx <kennyzx@4c95d191-cd10-4865-8b81-0088ca643448>
2013-09-13 12:30:30 +0000

    添加脚本：把需要的运行库（opencv. opengl, devil等）拷贝到Debug/Release目录
    git-svn-id: https://t.svnchina.com/svn/myuniverse/trunk@156 4c95d191-cd10-4865-8b81-0088ca643448

--- copy2output.bat
+++ copy2output.bat
@@ -1,1 +1,37 @@
-REM  copy the required binaries to the Debug and Release folder
+REM  copy the required binaries to the Release folder
+
+if exist src\Release echo "Release Folder already exists"
+if not exist src\Release mkdir src\Release
+
+xcopy InnoSetup\Files\* src\Release /y 
+
+REM  copy the required binaries to the Debugfolder
+
+if exist src\Debug echo "Debug Folder already exists"
+if not exist src\Debug mkdir src\Debug
+
+xcopy InnoSetup\Files\calibmain.ini src\Debug /y
+xcopy InnoSetup\Files\calibprev.ini src\Debug /y
+xcopy InnoSetup\Files\config.ini src\Debug /y
+
+xcopy InnoSetup\Files\DevIL.dll src\Debug /y
+xcopy InnoSetup\Files\iconv.dll src\Debug /y
+xcopy InnoSetup\Files\ILU.dll src\Debug /y
+xcopy InnoSetup\Files\ILUT.dll src\Debug /y
+xcopy InnoSetup\Files\libxml2.dll src\Debug /y
+xcopy InnoSetup\Files\MyUniverseControl.pptm src\Debug /y
+xcopy InnoSetup\Files\zlib1.dll src\Debug /y
+xcopy InnoSetup\Files\regMyUniverseControl.exe src\Debug /y
+
+xcopy 3rdparty\opencv231\bin\Debug\opencv_calib3d231d.dll src\Debug /y
+xcopy 3rdparty\opencv231\bin\Debug\opencv_core231d.dll src\Debug /y
+xcopy 3rdparty\opencv231\bin\Debug\opencv_features2d231d.dll src\Debug /y
+xcopy 3rdparty\opencv231\bin\Debug\opencv_flann231d.dll src\Debug /y
+xcopy 3rdparty\opencv231\bin\Debug\opencv_highgui231d.dll src\Debug /y
+xcopy 3rdparty\opencv231\bin\Debug\opencv_imgproc231d.dll src\Debug /y
+
+xcopy 3rdparty\Qt4.84\bin\QtCored4.dll src\Debug /y
+xcopy 3rdparty\Qt4.84\bin\QtGuid4.dll src\Debug /y
+xcopy 3rdparty\Qt4.84\bin\QtTestd4.dll src\Debug /y
+
+
