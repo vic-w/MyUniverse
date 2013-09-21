9c8ceffda1c484dc42f81f39bd9e1428b93fb900
airscrap <airscrap@4c95d191-cd10-4865-8b81-0088ca643448>
2013-09-21 08:40:00 +0000

    更新测试工程
    git-svn-id: https://t.svnchina.com/svn/myuniverse/trunk@158 4c95d191-cd10-4865-8b81-0088ca643448

--- src/ConsoleTest/main.cpp
+++ src/ConsoleTest/main.cpp
@@ -1,37 +1,44 @@
 ﻿#include "GlbCore.h"
-#include "GlbKt.h"
 
 GlbRotmat g_GlobeRotMat;
 GlbEularAngle g_GlobeEularAngle;
 
 void main()
 {
-    glbKt();
     GlbRect size(0,0,500,500);
-    glbCreateWindow(size, false,0);
-    glbInitDistort();
+    GlbWindow mainWindow;
+    glbCreateWindow(mainWindow, size, ".\\calibmain.ini", true, false);
 
     g_GlobeEularAngle.m_1_Horz = 0;
     g_GlobeEularAngle.m_2_Vert = 30;
     g_GlobeEularAngle.m_3_Axis = 0;
 
-
-    GlbImage Image = glbLoadImage("a001.dds");
-    //GlbImage Image = glbLoadImage("earth.jpg");
-
+    glbSwitchWindow(mainWindow);
+    GlbImage Image = glbLoadImage("earth.jpg");    
+    GlbImage Image2 = glbLoadImage("a001.dds");    
+    
     do
     {
         glbClearWindow();
-        glbDrawGlobe(Image, g_GlobeRotMat);
-        g_GlobeEularAngle.m_3_Axis += 0.2f;
+         g_GlobeEularAngle.m_3_Axis += 0.2f;
         if(g_GlobeEularAngle.m_3_Axis>360)
         {
             g_GlobeEularAngle.m_3_Axis -= 360;
         }
         glbEularAngle2Rotmat(g_GlobeEularAngle, g_GlobeRotMat);
-    }while(glbUpdateWindow(30));
+        glbDrawGlobe(Image, g_GlobeRotMat, mainWindow.m_calib);
+        GlbPointGeo p1(40,120);
+        GlbPointGeo p2(90,120);
+        GlbPoint3d p3;
+
+        glbDrawTexture(Image2, g_GlobeRotMat, mainWindow.m_calib, 
+            p1, true, p2, false, true, 180, 30, 1, GLB_TEX_BELT, p3);
+        glbDrawLineOnGlobe(p1, g_GlobeRotMat, mainWindow.m_calib, p2, 2);
+
+    }
+    while(glbUpdateWindow(mainWindow,30));
 
     glbReleaseImage(&Image);
-    glbDestoryWindow(0);
+    glbDestoryWindow(mainWindow);
     return;
 }
