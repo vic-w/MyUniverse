83d4047572824343ec7a9ea7b5ec1faef1835f82
kennyzx <kennyzx@4c95d191-cd10-4865-8b81-0088ca643448>
2013-10-10 10:14:20 +0000

    添加文字到图片的功能
    git-svn-id: https://t.svnchina.com/svn/myuniverse/trunk@228 4c95d191-cd10-4865-8b81-0088ca643448

--- src/text2ImageGenerator/Program.cs
+++ src/text2ImageGenerator/Program.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Drawing;
 using System.IO;
 
+
+
 namespace text2ImageGenerator
 {
     static class TextStyleCollection
@@ -15,36 +17,47 @@ namespace text2ImageGenerator
             textStyles = new Dictionary<string, TextStyle>();
             textStyles.Add("default", new TextStyle()
             {
-                atPoint = new Point(150, 50),
+                atPoint = new Point(10, 10),
                 color = Color.FromKnownColor(KnownColor.Red),
                 fontFamily = new FontFamily("Arial"),
                 fontSize = 32.0f,
                 fontStyle = FontStyle.Regular,
                 overAllAlignment = "BottomRight",
-                lineAlignment = StringAlignment.Center,
-                stringAlignment = StringAlignment.Center
+                lineAlignment = StringAlignment.Far,
+                stringAlignment = StringAlignment.Far
             });
             textStyles.Add("cityname", new TextStyle()
             {
-                atPoint = new Point(50, 50),
+                atPoint = new Point(10, 10),
                 color = Color.FromArgb(255, 255, 255, 255),
                 fontFamily = new FontFamily("Arial"),
                 fontSize = 40.0f,
                 fontStyle = FontStyle.Regular,
                 overAllAlignment = "TopLeft",
-                lineAlignment = StringAlignment.Far,
-                stringAlignment = StringAlignment.Far
+                lineAlignment = StringAlignment.Near,
+                stringAlignment = StringAlignment.Near
             });
             textStyles.Add("localtime", new TextStyle()
             { 
-                atPoint = new Point(20, 20),
+                atPoint = new Point(10, 10),
                 color = Color.FromArgb(255, 255, 255, 255),
                 fontFamily = new FontFamily("Arial"),
                 fontSize = 24.0f,
                 overAllAlignment = "TopRight",
                 fontStyle = FontStyle.Regular,
                 lineAlignment = StringAlignment.Near,
-                stringAlignment = StringAlignment.Near
+                stringAlignment = StringAlignment.Far
+            });
+            textStyles.Add("description", new TextStyle()
+            {
+                atPoint = new Point(10, 10),
+                color = Color.FromArgb(255, 255, 255, 255),
+                fontFamily = new FontFamily("Arial"),
+                fontSize = 20.0f,
+                fontStyle = FontStyle.Regular,
+                overAllAlignment = "BottomRight",
+                lineAlignment = StringAlignment.Far,
+                stringAlignment = StringAlignment.Far
             });
         }
     }
@@ -57,10 +70,17 @@ namespace text2ImageGenerator
         /// <param name="args"></param>
         static int Main(string[] args)
         {
+// TESTROUTINE
+            //SaveAsImage(TextStyleCollection.textStyles["description"], @"image\cities\beijing.jpg", "北京当前气温为22摄氏度北京当前气温为22摄氏度");
+            //System.Diagnostics.Process.Start("temp.jpg"); //open the image with photo viewer
+            //return 0;
+// TESTROUTINE end
+
+            System.Diagnostics.Stopwatch watch = System.Diagnostics.Stopwatch.StartNew();
             foreach (string arg in args)
                 System.Diagnostics.Trace.WriteLine("kennnyzx: " + arg);
 
-            if (args.Length != 1 && args.Length != 3)
+            if (args.Length == 0)
                 return -1;
 
             else if (args.Length == 1)
@@ -69,12 +89,28 @@ namespace text2ImageGenerator
                 string myString = args[0];
                 return -1;
             }
-            else if (args.Length == 3)
+            else if (args.Length == 2)
             {
                 string bgImageFile = args[0];
                 if (File.Exists(bgImageFile))
                 {
-                    string myString = args[2];
+                    string myString = args[1];
+                    SaveAsImage(TextStyleCollection.textStyles["default"], bgImageFile, myString);
+                    return 0;
+                }
+                else
+                    return -1;
+            }
+            else
+            {
+                string bgImageFile = args[0];
+                if (File.Exists(bgImageFile))
+                {
+                    StringBuilder sbMyString = new StringBuilder();
+                    for (int index = 2; index < args.Length; index++)
+                        sbMyString.Append(args[index]);
+                    string myString = sbMyString.ToString();
+
                     switch (args[1])
                     {
                         case "1":
@@ -92,15 +128,19 @@ namespace text2ImageGenerator
                         case "5":
                             SaveAsImage(TextStyleCollection.textStyles["default"], bgImageFile, myString);
                             break;
+                        default:
+                             SaveAsImage(TextStyleCollection.textStyles["default"], bgImageFile, myString);
+                             break;
                     }
-
+                    System.Diagnostics.Trace.WriteLine(watch.ElapsedMilliseconds);
                     return 0;
                 }
                 else
+                {
+                    System.Diagnostics.Trace.WriteLine(watch.ElapsedMilliseconds);
                     return -1;
+                }
             }
-            else
-                return -1;
         }
 
         static void SaveAsImage(TextStyle textStyle, string bgImageFileName, string myString)
@@ -108,17 +148,22 @@ namespace text2ImageGenerator
             using (System.Drawing.Image bitmap = (System.Drawing.Image)Bitmap.FromFile(bgImageFileName))
             {
                 Font font = new Font(textStyle.fontFamily, textStyle.fontSize, textStyle.fontStyle, GraphicsUnit.Pixel);
+                Graphics graphics = Graphics.FromImage(bitmap);
+                SizeF sizeRequired = graphics.MeasureString(myString, font, bitmap.Width - 2 * textStyle.atPoint.X);
+                if (sizeRequired.Height > (bitmap.Height / 2 - textStyle.atPoint.Y))
+                {
+                    //more space is required.
+                }
 
                 Point atpoint;
-                atpoint = new Point(textStyle.atPoint.X, textStyle.atPoint.Y);
-                //if (textStyle.overAllAlignment == "TopLeft")
-                //    atpoint = new Point(textStyle.atPoint.X, textStyle.atPoint.Y);
-                //else if (textStyle.overAllAlignment == "TopRight")
-                //    atpoint = new Point(bitmap.Width - textStyle.atPoint.X, textStyle.atPoint.Y);
-                //else if (textStyle.overAllAlignment == "BottomLeft")
-                //    atpoint = new Point(textStyle.atPoint.X, bitmap.Height - textStyle.atPoint.Y);
-                //else 
-                //    atpoint = new Point(bitmap.Width - textStyle.atPoint.X, bitmap.Height - textStyle.atPoint.Y);
+                if (textStyle.overAllAlignment == "TopLeft")
+                    atpoint = new Point(textStyle.atPoint.X, textStyle.atPoint.Y);
+                else if (textStyle.overAllAlignment == "TopRight")
+                    atpoint = new Point(bitmap.Width - textStyle.atPoint.X, textStyle.atPoint.Y);
+                else if (textStyle.overAllAlignment == "BottomLeft")
+                    atpoint = new Point(textStyle.atPoint.X, bitmap.Height - textStyle.atPoint.Y);
+                else
+                    atpoint = new Point(bitmap.Width - textStyle.atPoint.X, bitmap.Height - textStyle.atPoint.Y);
 
                 SolidBrush brush = new SolidBrush(textStyle.color);
 
@@ -126,7 +171,6 @@ namespace text2ImageGenerator
                 sf.Alignment = textStyle.stringAlignment;
                 sf.LineAlignment = textStyle.lineAlignment;
 
-                Graphics graphics = Graphics.FromImage(bitmap);
                 graphics.DrawString(myString, font, brush, atpoint, sf);
                 graphics.Dispose();
                 using (FileStream fs = new FileStream("temp.jpg", FileMode.Create, FileAccess.Write))
