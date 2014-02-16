93abf222c157d92d78f513440d3edf381057c996
airscrap <airscrap@4c95d191-cd10-4865-8b81-0088ca643448>
2014-02-16 03:57:21 +0000

    忽略一些文件
    git-svn-id: https://t.svnchina.com/svn/myuniverse/trunk@309 4c95d191-cd10-4865-8b81-0088ca643448

---
+++ test/TUIO_CLIENT_test/TUIO_CLIENT_test/main.cpp
@@ -0,0 +1,66 @@
+#include "TuioListener.h"
+#include "TuioClient.h"
+
+using namespace TUIO;
+
+class TuioDump : public TuioListener {
+	
+	public:
+		void addTuioObject(TuioObject *tobj);
+		void updateTuioObject(TuioObject *tobj);
+		void removeTuioObject(TuioObject *tobj);
+
+		void addTuioCursor(TuioCursor *tcur);
+		void updateTuioCursor(TuioCursor *tcur);
+		void removeTuioCursor(TuioCursor *tcur);
+
+		void refresh(TuioTime frameTime);
+};
+
+void TuioDump::addTuioObject(TuioObject *tobj) {
+	std::cout << "add obj " << tobj->getSymbolID() << " (" << tobj->getSessionID() << ") "<< tobj->getX() << " " << tobj->getY() << " " << tobj->getAngle() << std::endl;
+}
+
+void TuioDump::updateTuioObject(TuioObject *tobj) {
+	std::cout << "set obj " << tobj->getSymbolID() << " (" << tobj->getSessionID() << ") "<< tobj->getX() << " " << tobj->getY() << " " << tobj->getAngle() 
+				<< " " << tobj->getMotionSpeed() << " " << tobj->getRotationSpeed() << " " << tobj->getMotionAccel() << " " << tobj->getRotationAccel() << std::endl;
+}
+
+void TuioDump::removeTuioObject(TuioObject *tobj) {
+	std::cout << "del obj " << tobj->getSymbolID() << " (" << tobj->getSessionID() << ")" << std::endl;
+}
+
+void TuioDump::addTuioCursor(TuioCursor *tcur) {
+	std::cout << "add cur " << tcur->getCursorID() << " (" <<  tcur->getSessionID() << ") " << tcur->getX() << " " << tcur->getY() << std::endl;
+}
+
+void TuioDump::updateTuioCursor(TuioCursor *tcur) {
+	std::cout << "set cur " << tcur->getCursorID() << " (" <<  tcur->getSessionID() << ") " << tcur->getX() << " " << tcur->getY() 
+				<< " " << tcur->getMotionSpeed() << " " << tcur->getMotionAccel() << " " << std::endl;
+}
+
+void TuioDump::removeTuioCursor(TuioCursor *tcur) {
+	std::cout << "del cur " << tcur->getCursorID() << " (" <<  tcur->getSessionID() << ")" << std::endl;
+}
+
+void  TuioDump::refresh(TuioTime frameTime) {
+	//std::cout << "refresh " << frameTime.getTotalMilliseconds() << std::endl;
+}
+
+int main(int argc, char* argv[])
+{
+	if( argc >= 2 && strcmp( argv[1], "-h" ) == 0 ){
+        	std::cout << "usage: TuioDump [port]\n";
+        	return 0;
+	}
+
+	int port = 3333;
+	if( argc >= 2 ) port = atoi( argv[1] );
+
+	TuioDump dump;
+	TuioClient client(port);
+	client.addTuioListener(&dump);
+	client.connect(true);
+
+	return 0;
+}
